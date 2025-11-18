4f4ac81bcc088e7fefcbe442be43a6067289e020
Brenda Vogel <breenvogel07@gmail.com>
2025-11-18 17:54:42 -0300

    adicionando correlção bb

---
+++ Brenda/correla_BB.py
@@ -0,0 +1,23 @@
+import numpy 
+
+#dados:
+Vli_std=10000
+D_=6                 #pol
+P_montante_choke=15  #bar
+L_anm=950        #m
+inclinacao=37
+L_manifold=850
+Te_Well=80 
+Ti_well=80      #ºC
+T_Succao=4       #ºC
+T_manifold=4     #ºC
+T_sup=17      #ºC
+Bsw=0.30
+RGL=150  #sm³/sm³
+API=25
+dg=0.75
+P_res=550           #bar
+tensao_og=0.00841   #N/m
+tensao_wg=0.004 #N/m
+TEC_w=2  #w/mK
+TEC_amb=1      #w/mK
